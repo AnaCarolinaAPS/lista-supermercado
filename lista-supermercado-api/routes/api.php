bde0e6c654dc6289f259b6f18b2f50eaca2f73c5
Ana Carolina <anacarolinaaps@gmail.com>
2025-11-28 23:26:17 -0300

    feat(api/Products) Controller + Route
    - Create ProductController with Index + Show functions;
    - Update routes/api.php with the api route for ProductController;

--- lista-supermercado-api/routes/api.php
+++ lista-supermercado-api/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\SupermarketController;
 
 Route::apiResource('categories', CategoryController::class);
 Route::apiResource('supermarkets', SupermarketController::class);
+Route::apiResource('products', ProductController::class);
